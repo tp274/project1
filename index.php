db1c3c68c96e02f4880539a11fa8cab2f75d9ef6
Trupti Jagdish Patel <tp274@afsconnect1.njit.edu>
2017-10-18 13:07:37 -0400

    testing

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+require _DIR_ . '/Actions.php';
+/*$actions = new Actions($_REQUEST);
+
+$action = isset($_GET['action']) ? _get[action] : 'form';
+switch($action){
+case 'form':
+case 'submit':
+case 'display':
+  $actions->$action();
+    break;
+    default:
+      echo 'Invalid action';
+        break;
+	}*/
+?>

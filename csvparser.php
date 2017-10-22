b38363394a21460899159e089042540f5ad6ccc2
Trupti Jagdish Patel <tp274@afsconnect1.njit.edu>
2017-10-22 12:23:40 -0400

    removed white space lines

--- csvparser.php
+++ csvparser.php
@@ -11,6 +11,8 @@ class CsvParser extends Parser{
 	//Parse the input file and returns array 
 	public function parse($filename){
 	     $lines = file($filename);
+	     $lines = array_map('trim',$lines);
+	     $lines = array_filter($lines);
 	     $data = array_map('str_getcsv', $lines);
 	     return $data;
 	}
